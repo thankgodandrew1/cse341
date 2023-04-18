25017a8d9459e0c46ca38460a7f25577ae842908
thankgodandrew1 <and22046@byui.edu>
2023-04-18 16:06:43 -0600

    fixed render hosting error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 // Create a new instance of the express application, const for port and hostname
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const hostname = '127.0.0.1';
 
 // Define a route that will return the name "Precious Jasper" when requested
