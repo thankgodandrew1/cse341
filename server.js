c8e52253bf819c86c760c8635546ee9a971df130
thankgodandrew1 <and22046@byui.edu>
2023-05-11 17:32:06 -0600

    added code to test on frontend

--- server.js
+++ server.js
@@ -25,16 +25,23 @@ db.connect()
     // use the home route for all other routes
     app.use('/', homeRoute);
 
+    app.use(bodyParser.json());
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+      );
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      next();
+    });
+
     app.use((req, res, next) => {
       const error = new Error('Not found');
       error.status = 404;
       next(error);
     });
-    app.use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
     app.use((error, req, res, next) => {
       res.status(error.status || 500);
       res.json({
